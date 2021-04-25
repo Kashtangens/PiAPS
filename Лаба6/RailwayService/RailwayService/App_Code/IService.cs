14415c26e05e1015fc2c7b787bda6988b520c07f
Kashtangens <kashtan_kashtanow@mail.ru>
2021-04-25 23:28:19 +0300

    Start lab6

---
+++ Лаба6/RailwayService/RailwayService/App_Code/IService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Реструктуризация" можно использовать для одновременного изменения имени интерфейса "IService" в коде и файле конфигурации.
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	string GetData(int value);
+
+	[OperationContract]
+	CompositeType GetDataUsingDataContract(CompositeType composite);
+
+	// TODO: Добавьте здесь операции служб
+	// Посмотреть все рейсы по заданному маршруту
+	[OperationContract]
+	List<Trip> GetTrip(string wayName);
+
+	// Посмотреть наличие билетов
+	[OperationContract]
+	int GetTicketsCount(Trip trip);
+
+	// Заказать билет на рейс
+	[OperationContract]
+	bool BuyTicketOnTrip(Trip trip);
+}
+
+// Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+[DataContract]
+public class CompositeType
+{
+	bool boolValue = true;
+	string stringValue = "Hello ";
+
+	[DataMember]
+	public bool BoolValue
+	{
+		get { return boolValue; }
+		set { boolValue = value; }
+	}
+
+	[DataMember]
+	public string StringValue
+	{
+		get { return stringValue; }
+		set { stringValue = value; }
+	}
+}
+
+[DataContract]
+public class Trip
+{
+	int wayNumber;
+	int ticketsCount;
+
+	[DataMember]
+	public int WayNumber
+	{
+		get { return wayNumber; }
+		set { wayNumber = value; }
+	}
+
+	[DataMember]
+	public int TicketsCount
+    {
+		get { return ticketsCount; }
+		set { ticketsCount = value; }
+    }
+
+	public Trip(int wayNumber, int ticketsCount)
+    {
+		this.wayNumber = wayNumber;
+		this.ticketsCount = ticketsCount;
+    }
+}
+
+[DataContract]
+public class Way
+{
+	int wayNumber;
+	string wayName;
+
+	[DataMember]
+	public int WayNumber
+    { 
+		get { return wayNumber; }
+		set { wayNumber = value; } 
+	}
+
+	[DataMember]
+	public string WayName
+	{
+		get { return wayName; }
+		set { wayName = value; }
+	}
+
+
+	public Way(int wayNumber, string wayName)
+    {
+		this.wayNumber = wayNumber;
+		this.wayName = wayName;
+    }
+}
+
+
---
+++ Лаба6/RailwayService/RailwayService/App_Code/Service.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Реструктуризация" можно использовать для одновременного изменения имени класса "Service" в коде, SVC-файле и файле конфигурации.
+public class Service : IService
+{
+	//инициализация маршрутов
+	Way[] ways =
+	{
+		new Way(0, "Moscow-Spb"),
+		new Way(1, "Spb-Kaliningrad"),
+		new Way(2, "Kaliningrad-Moscow")
+	};
+
+	// инициализация рейсов
+	Trip[] trips =
+	{
+		new Trip(0, 10),
+		new Trip(0, 50),
+		new Trip(1, 2),
+		new Trip(2, 12),
+		new Trip(2, 0)
+	};
+
+
+	public string GetData(int value)
+	{
+		return string.Format("You entered: {0}", value);
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+
+	/// <summary>
+	/// Функция получения количества билета по рейсу
+	/// </summary>
+	/// <param name="trip">Рейс</param>
+	/// <returns>Количество билетов</returns>
+    public int GetTicketsCount(Trip trip)
+    {
+        throw new NotImplementedException();
+    }
+
+	/// <summary>
+	/// Функция получения списка рейсов по номеру пути
+	/// </summary>
+	/// <param name="wayName"></param>
+	/// <returns></returns>
+    public List<Trip> GetTrip(string wayName)
+    {
+        throw new NotImplementedException();
+    }
+
+	/// <summary>
+	/// Функция покупки билета на рейс
+	/// </summary>
+	/// <param name="trip">Рейс</param>
+	/// <returns>
+	/// true - если успешно купил билет;
+	/// false - если билет купить не удалось.
+	/// </returns>
+	public bool BuyTicketOnTrip(Trip trip)
+	{
+		throw new NotImplementedException();
+	}
+}
